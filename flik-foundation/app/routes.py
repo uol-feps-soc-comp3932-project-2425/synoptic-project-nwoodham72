a56f2830c1686111ba95a1789a33b9a7f3489a3b
Nathan Woodham <sc21nw@leeds.ac.uk>
2025-03-31 19:34:58 +0100

    style: centered flash text

--- flik-foundation/app/routes.py
+++ flik-foundation/app/routes.py
@@ -2,13 +2,20 @@ from flask import Blueprint, render_template, redirect, url_for, flash, request
 from flask_login import login_required, current_user
 from .forms import RaiseBugForm
 from azure_integration.client import create_work_item
-from azure_integration.config import PROJECT_NAME, ORGANISATION_URL, ORGANISATION, PERSONAL_ACCESS_TOKEN, RETRIEVAL_ACCESS_TOKEN
+from azure_integration.config import (
+    PROJECT_NAME,
+    ORGANISATION_URL,
+    ORGANISATION,
+    PERSONAL_ACCESS_TOKEN,
+    RETRIEVAL_ACCESS_TOKEN,
+)
 from bert.summariser import extractive_summary
 from bert.prioritiser import predict_priority
 from bert.assigner import assign_developer
 from bert.assessor import assess_documentation
 
 main = Blueprint("main", __name__)
+
 
 @main.route("/dashboard")
 @login_required
@@ -23,60 +30,83 @@ def dashboard():
         flash("Unauthorised access", "danger")
         return redirect(url_for("main.index"))
 
+
 @main.route("/", methods=["GET"])
 def index():
     return redirect(url_for("main.raise_bug"))
+
 
 # List user accounts for testing
 @main.route("/users")
 def list_users():
     from .models import User
+
     users = User.query.all()
     return "<br>".join([f"{u.id} | {u.email} | {u.role}" for u in users])
+
 
 @main.route("/raise_bug", methods=["GET", "POST"])
 @login_required
 def raise_bug():
     form = RaiseBugForm()
-    bug_details = None 
+    bug_details = None
     additional_comments = None
     matching_docs = None
     tags = []
 
     if form.validate_on_submit():
         # Check for additional comments from documentation match modal
-        additional_comments = request.form.get('additional_comments', '').strip()
-    
+        additional_comments = request.form.get("additional_comments", "").strip()
+
         # Get form details
         bug_details = {
-            'title': form.title.data,
-            'role': form.role.data,
-            'page': form.page.data,
-            'description': form.description.data,
-            'expected': form.expected.data,
+            "title": form.title.data,
+            "role": form.role.data,
+            "page": form.page.data,
+            "description": form.description.data,
+            "expected": form.expected.data,
         }
-
 
         # Check for additioal comments
         if not additional_comments:
             # Generate documentation similarity
             prep_documentation_comparison = (
-                "I am a " + bug_details['role'].strip() + "user on the " + bug_details['page'].strip() + "page.\n" +
-                bug_details['description'].strip() + ".\n" +      
-                bug_details['expected'].strip() + ".\n" 
+                "I am a "
+                + bug_details["role"].strip()
+                + "user on the "
+                + bug_details["page"].strip()
+                + "page.\n"
+                + bug_details["description"].strip()
+                + ".\n"
+                + bug_details["expected"].strip()
+                + ".\n"
             )
             # Return matching documentation
-            match, matching_docs = assess_documentation(prep_documentation_comparison, bug_details['role'].strip())
+            match, matching_docs = assess_documentation(
+                prep_documentation_comparison, bug_details["role"].strip()
+            )
             if match and matching_docs:
-                return render_template("raise_bug.html", form=form, bug_details=None, matching_docs=matching_docs)  # Display matching documentation
-        
+                return render_template(
+                    "raise_bug.html",
+                    form=form,
+                    bug_details=None,
+                    matching_docs=matching_docs,
+                )  # Display matching documentation
+
         # Generate extractive summary of bug ticket
         prep_summary_data = (
-            bug_details['title'].strip() + ".\n" +  # Add full stops to end of each step
-            f"I am a " + bug_details['role'].strip() + "user.\n" +  
-            f"I am on the " + bug_details['page'].strip() + "page.\n" +
-            bug_details['description'].strip() + ".\n" +      
-            bug_details['expected'].strip() + ".\n" 
+            bug_details["title"].strip()
+            + ".\n"  # Add full stops to end of each step
+            + f"I am a "
+            + bug_details["role"].strip()
+            + "user.\n"
+            + f"I am on the "
+            + bug_details["page"].strip()
+            + "page.\n"
+            + bug_details["description"].strip()
+            + ".\n"
+            + bug_details["expected"].strip()
+            + ".\n"
         )
 
         summary = extractive_summary(prep_summary_data)
@@ -98,40 +128,77 @@ def raise_bug():
 
         # Fetch developers and skills
         developers = {
-            "nathanmw72@gmail.com": {"Information", "Feedback", "Sales", "Infrastructure", "IT"},
-            "sc21nw@leeds.ac.uk": {"Login", "Feedback", "Sales"}
+            "nathanmw72@gmail.com": {
+                "Information",
+                "Feedback",
+                "Sales",
+                "Infrastructure",
+                "IT",
+            },
+            "sc21nw@leeds.ac.uk": {"Login", "Feedback", "Sales"},
         }
 
-        assigned_to, tags = assign_developer(developers, prep_summary_data, ORGANISATION, PROJECT_NAME, RETRIEVAL_ACCESS_TOKEN)
-        if additional_comments: tags.append("Documentation Misalignment") 
+        assigned_to, tags = assign_developer(
+            developers,
+            prep_summary_data,
+            ORGANISATION,
+            PROJECT_NAME,
+            RETRIEVAL_ACCESS_TOKEN,
+        )
+        if additional_comments:
+            tags.append("Documentation Misalignment")
         structured_tags = ", ".join(tags) if tags else "Miscellaneous"
-
 
         try:
             work_item = create_work_item(
                 PROJECT_NAME,
-                bug_details['title'],
+                bug_details["title"],
                 description,
                 priority_level,
                 assigned_to,
-                structured_tags
+                structured_tags,
             )
-            flash(f"Ticket created with ID: {work_item.id}", "success")
+
+            # Successful output
+            if additional_comments:  # Success message on documentation override
+                flash(
+                    """
+                    <div class='text-center'>
+                        <img src='/static/images/bug.png' alt='Bug Logo' class='img-fluid mb-2' style='max-width: 50px;'>
+                        <h5 class='alert-heading'>It's a bug!</h5>
+                        <p>
+                        Your bug has been sent to the development team to be resolved. The development team have been flagged of the documentation issues and will investigate this further.
+                        Please note that the development team may reach out to you for further details if required.
+                        </p>
+                    </div>
+                    """,
+                    "success",
+                )
+            # Standard success message
+            else:
+                # Bug Logo: Bootstrap 5.2 images (https://icons.getbootstrap.com/)
+                flash(
+                    """
+                    <div class='text-center'>
+                        <img src='/static/images/bug.png' alt='Bug Logo' class='img-fluid mb-2' style='max-width: 50px;'> 
+                        <h5 class='alert-heading'>It's a bug!</h5>
+                        <p>
+                        Your bug has been sent to the development team to be resolved. Thank you for taking the time to report this bug through Flik.
+                        Please note that the development team may reach out to you for further details if required.
+                        </p>
+                    </div>
+                    """,
+                    "success",
+                )
 
             # Clear form after successful send
-            form.title.data = ''
-            form.role.data = ''
-            form.page.data = ''
-            form.description.data = ''
-            form.expected.data = ''
+            form.title.data = ""
+            form.role.data = ""
+            form.page.data = ""
+            form.description.data = ""
+            form.expected.data = ""
 
         except Exception as e:
             flash(f"Failed to create ticket: {e}", "danger")
 
-        # If you want to stay on the same page (and show bug_details), DON'T redirect
-        # If you do redirect, you'll lose bug_details unless you store it in session or flash
-        # return redirect(url_for("main.raise_bug"))
-
-    # 3. Now bug_details is either None or a dict, but it always exists
     return render_template("raise_bug.html", form=form, bug_details=bug_details)
-
