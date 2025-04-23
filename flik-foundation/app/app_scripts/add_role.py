b869654e76d7930c290f133c5596ed20aa23ddd7
Nathan Woodham <sc21nw@leeds.ac.uk>
2025-04-23 13:48:49 +0100

    feat: added deleted role

--- flik-foundation/app/app_scripts/add_role.py
+++ flik-foundation/app/app_scripts/add_role.py
@@ -4,8 +4,8 @@ from app.models import FlikRole
 app = create_app()
 app.app_context().push()
 
-new_role = FlikRole(name='Manager')
+new_role = FlikRole(name='Deleted')
 
 db.session.add(new_role)
 db.session.commit()
-print('Added new role: Manager')
+print('Added new role: Deleted')
